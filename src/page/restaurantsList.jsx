22da8eb278a074f91f0479308e84e7644f76bc20
강란규 <bpos333@gmail.com>
2024-11-29 15:57:23 +0900

    라우터 경로수정

---
+++ src/page/restaurantsList.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const RestaurantsList = () => {
+    return (
+        <div>restaurantsList</div>
+    )
+}
+
+export default RestaurantsList
--- src/shared/Router.jsx
+++ src/shared/Router.jsx
@@ -1,14 +1,18 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-
+import Home from "../page/Home";
+import Login from "../page/Login";
+import Singup from "../page/Singup";
+import MyPage from "../page/myPage";
+import RestaurantsList from "../page/restaurantsList";
 function Router() {
     return (
         <BrowserRouter>
             <Routes>
-                <Route path="/" element={<>홈</>} />
-                <Route path="/login" element={<>로그인</>} />
-                <Route path="/signup" element={<>회원가입</>} />
-                <Route path="/mypage" element={<>마이페이지</>} />
-                <Route path="/restaurants/:id" element={<>상세 페이지</>} />
+                <Route path="/" element={<Home />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Singup />} />
+                <Route path="/mypage" element={<MyPage />} />
+                <Route path="/restaurants/:id" element={<RestaurantsList />} />
             </Routes>
         </BrowserRouter>
     );
