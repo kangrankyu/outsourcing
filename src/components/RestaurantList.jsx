aa765a2d75c878d1166334ae7017b509f9b9f973
ovezen <klimen1005@gmail.com>
2024-12-04 12:05:45 +0900

    Feat: 음식점 이름과 주소 검색 기능 추가

--- src/components/RestaurantList.jsx
+++ src/components/RestaurantList.jsx
@@ -29,28 +29,46 @@ const MoreButton = styled.button`
 
 const RestaurantList = ({ onSelectRestaurant }) => {
   const { data: restaurants, isLoading, error } = useRestaurantsData();
-  const [ visibleCount, setVisibleCount ] = useState(5);
+  const [visibleCount, setVisibleCount] = useState(5);
+  const [searchQuery, setSearchQuery] = useState('');
 
   if (isLoading) return <div>Loading...</div>;
   if (error) return <div>Error: {error.message}</div>;
+
+  // 검색어를 기준으로 필터링하기 (null 체크 추가)
+  const filteredRestaurants = restaurants.filter(
+    (restaurant) =>
+      restaurant.name?.includes(searchQuery) ||
+      restaurant.address?.includes(searchQuery)
+  );
 
   // 더보기 버튼 클릭 시 표시할 항목 수 + 5
   const handleMoreClick = () => {
     setVisibleCount((prevCount) => prevCount + 5);
   };
+
   return (
     <ListWrapper>
+      <input
+        type="text"
+        placeholder="음식점 이름 또는 주소 검색"
+        value={searchQuery}
+        onChange={(e) => {
+          setSearchQuery(e.target.value);
+          setVisibleCount(5);
+        }}
+      />
+
       <div>
-        {restaurants.slice(0, visibleCount).map((restaurant) => (
+        {filteredRestaurants.slice(0, visibleCount).map((restaurant) => (
           <ListItem key={restaurant.id} onClick={() => onSelectRestaurant(restaurant)}>
             <p>{restaurant.name}</p>
             <p>{restaurant.address}</p>
           </ListItem>
         ))}
       </div>
-      {visibleCount < restaurants.length && (
-        <MoreButton onClick={handleMoreClick}>더 보기</MoreButton>
-      )}
+
+      {visibleCount < filteredRestaurants.length && <MoreButton onClick={handleMoreClick}>더 보기</MoreButton>}
     </ListWrapper>
   );
 };
